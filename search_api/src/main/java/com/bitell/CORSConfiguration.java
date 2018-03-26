895c7c800bcc20600f4084d9e9ce0f57d43e15c1
huangan <270753933@qq.com>
2018-03-26 15:48:32 +0800

    查询服务架构搭建

---
+++ search_api/src/main/java/com/bitell/CORSConfiguration.java
@@ -0,0 +1,28 @@
+package com.bitell;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+* Company:千山健康
+* Author:an.huang
+* Date:2018/1/19
+*/
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
